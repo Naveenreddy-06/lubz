a2ef9b4415757611c122efecbfc1995ed86505b5
Naveenreddy-06 <burranaveenreddy79@gmail.com>
2025-08-29 13:01:41 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -11,7 +11,8 @@ System.out.println("Addition:" + calc.add(10,5));
 System.out.println("Subtraction:" + calc.subtract(10,5));
 System.out.println("Multiplication:" + calc.multiply(10,5));
 System.out.println("Division:" + calc.divide(10,5));
-}}
+}
+}
 
 
 	
