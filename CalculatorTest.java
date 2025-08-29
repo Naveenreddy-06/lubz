ad286864619a72c79fe61db4443889f78180cf4f
Naveenreddy-06 <burranaveenreddy79@gmail.com>
2025-08-29 13:18:03 +0530

    Update CalculatorTest.java

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -5,19 +5,23 @@ public class CalculatorTest
 {
     Calculator calc = new Calculator();
 
-    @Test public void testAddition() {
+    @Test public void testAddition() 
+    {
         assertEquals(15, calc.add(10, 5));
     }
 
-    @Test public void testSubtraction() {
+    @Test public void testSubtraction() 
+    {
         assertEquals(5, calc.subtract(10, 5));
     }
 
-    @Test public void testMultiplication() {
+    @Test public void testMultiplication()
+    {
         assertEquals(50, calc.multiply(10, 5));
     }
 
-    @Test public void testDivision() {
+    @Test public void testDivision()
+    {
         assertEquals(2, calc.divide(10, 5));
     }
 }
